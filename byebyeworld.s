027e3c647cc911334d16484120120261fe2a12e3
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2019-11-07 12:09:59 -0500

    Final commit :sunglasses:

--- byebyeworld.s
+++ byebyeworld.s
@@ -1,1 +1,14 @@
-6de73a8e-0181-11ea-81eb-60f81dc4174c
+; 32-bit Linux implementation
+section     .text
+global      _start
+_start:
+    mov     edx,len
+    mov     ecx,msg
+    mov     ebx,1 ; stdout
+    mov     eax,4 ; sys_write id
+    int     0x80
+    mov     eax,1 ; exit id
+    int     0x80
+section     .data
+msg     db  'Hello, world!',0xa,0x0 ;not sure if null needed, might as well as have it...
+len     equ $ - msg
